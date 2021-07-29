9c136cbcd75d4bcbb5c936f79793deb21d8d4999
Axel Jorgensen <code@axeljorgensen.info>
2021-07-29 16:02:30 -0700

    styled checklists

--- src/Components/Todo/TodoItem/TodoItem.js
+++ src/Components/Todo/TodoItem/TodoItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './todoItem.css'
 
 function TodoItem() {
     return (
         <div className='todo-item'>
-            <input id='todo1' name='todo1' type='checkbox' />
-            <label htmlFor='todo1'>Insert Todo here</label>
+            <div className='todo-item-container'>
+                <input id='todo1' name='todo1' type='checkbox' />
+                <label htmlFor='todo1'><span className='checkmark'>Insert Todo here I wqonder what will happoen if its relaly l,nong</span></label>
+            </div>
+            <div className='todo-item-container'>
+                <input id='todo2' name='todo2' type='checkbox' />
+                <label htmlFor='todo2'><span className='checkmark'>Insert Todo here</span></label>
+            </div>
         </div>
     )
 }
