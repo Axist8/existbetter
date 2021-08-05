15df8b7048537cd55a5937c4cded9051203dbf6b
Axel Jorgensen <code@axeljorgensen.info>
2021-08-04 23:24:38 -0700

    Created all base structure for all components

---
+++ src/Components/ActionPlate/Routines/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Routines = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Routines.propTypes = {
+
+};
+
+export default Routines;
